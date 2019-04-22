32f03bedec6d6d2597997975b2a55effe3c2daa1
Hardik Gupta <guptahardik17@gmail.com>
2019-04-22 21:55:50 +0530

    Update dbconn.js

--- config/dbconn.js
+++ config/dbconn.js
@@ -1,12 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-	supportBigNumbers: true,
-	bigNumberStrings: true,
-	// host: '23.94.69.34',
-  // user: 'prayaasc_scapic',
-  // password : 'scapic',
-  // database : 'prayaasc_scapic'
-	host: 'localhost',
+  supportBigNumbers: true,
+  bigNumberStrings: true,
+  host: 'localhost',
   user: 'root',
   password : 'root',
   database : 'scapic'
